941d48956d139c36a372ff697e610ad3c7b4788c
Shubham Kshirsagar <103346490+shubham0454@users.noreply.github.com>
2024-08-26 23:07:55 +0530

    Add files via upload

---
+++ Employee PayRoll/Form1.Designer.cs
@@ -0,0 +1,380 @@
+﻿namespace Employee_PayRoll
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.IdtextBox = new System.Windows.Forms.TextBox();
+            this.NametextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.DestextBox = new System.Windows.Forms.TextBox();
+            this.BasictextBox = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.ContextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.MedtextBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.HousetextBox = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.GrosstextBox = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.IncometextBox = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.NettextBox = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider2 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider3 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider4 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(252, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Employee PayRoll";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(239, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Id";
+            // 
+            // IdtextBox
+            // 
+            this.IdtextBox.Location = new System.Drawing.Point(348, 60);
+            this.IdtextBox.Name = "IdtextBox";
+            this.IdtextBox.Size = new System.Drawing.Size(160, 20);
+            this.IdtextBox.TabIndex = 0;
+            this.IdtextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.IdtextBox.Leave += new System.EventHandler(this.textBox1_Leave);
+            // 
+            // NametextBox
+            // 
+            this.NametextBox.Location = new System.Drawing.Point(348, 86);
+            this.NametextBox.Name = "NametextBox";
+            this.NametextBox.Size = new System.Drawing.Size(160, 20);
+            this.NametextBox.TabIndex = 1;
+            this.NametextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.NametextBox.Leave += new System.EventHandler(this.NametextBox_Leave);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(239, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Name";
+            // 
+            // DestextBox
+            // 
+            this.DestextBox.Location = new System.Drawing.Point(348, 116);
+            this.DestextBox.Name = "DestextBox";
+            this.DestextBox.Size = new System.Drawing.Size(160, 20);
+            this.DestextBox.TabIndex = 2;
+            this.DestextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.DestextBox.Layout += new System.Windows.Forms.LayoutEventHandler(this.DestextBox_Layout);
+            this.DestextBox.Leave += new System.EventHandler(this.DestextBox_Leave);
+            // 
+            // BasictextBox
+            // 
+            this.BasictextBox.Location = new System.Drawing.Point(348, 142);
+            this.BasictextBox.Name = "BasictextBox";
+            this.BasictextBox.Size = new System.Drawing.Size(160, 20);
+            this.BasictextBox.TabIndex = 3;
+            this.BasictextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.BasictextBox.Leave += new System.EventHandler(this.BasictextBox_Leave);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(348, 116);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(160, 20);
+            this.textBox5.TabIndex = 2;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(239, 117);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Designation";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(348, 142);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(160, 20);
+            this.textBox7.TabIndex = 2;
+            this.textBox7.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(348, 142);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(160, 20);
+            this.textBox8.TabIndex = 2;
+            this.textBox8.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(239, 143);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 16);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Basic Pay";
+            // 
+            // ContextBox
+            // 
+            this.ContextBox.Location = new System.Drawing.Point(348, 168);
+            this.ContextBox.Name = "ContextBox";
+            this.ContextBox.ReadOnly = true;
+            this.ContextBox.Size = new System.Drawing.Size(160, 20);
+            this.ContextBox.TabIndex = 4;
+            this.ContextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(239, 168);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(93, 16);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Conveyance";
+            // 
+            // MedtextBox
+            // 
+            this.MedtextBox.Location = new System.Drawing.Point(348, 200);
+            this.MedtextBox.Name = "MedtextBox";
+            this.MedtextBox.ReadOnly = true;
+            this.MedtextBox.Size = new System.Drawing.Size(160, 20);
+            this.MedtextBox.TabIndex = 5;
+            this.MedtextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(239, 200);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(62, 16);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "Medical";
+            // 
+            // HousetextBox
+            // 
+            this.HousetextBox.Location = new System.Drawing.Point(348, 226);
+            this.HousetextBox.Name = "HousetextBox";
+            this.HousetextBox.ReadOnly = true;
+            this.HousetextBox.Size = new System.Drawing.Size(160, 20);
+            this.HousetextBox.TabIndex = 6;
+            this.HousetextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(239, 226);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(88, 16);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "House Rent";
+            // 
+            // GrosstextBox
+            // 
+            this.GrosstextBox.Location = new System.Drawing.Point(348, 252);
+            this.GrosstextBox.Name = "GrosstextBox";
+            this.GrosstextBox.ReadOnly = true;
+            this.GrosstextBox.Size = new System.Drawing.Size(160, 20);
+            this.GrosstextBox.TabIndex = 7;
+            this.GrosstextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(239, 252);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(79, 16);
+            this.label9.TabIndex = 3;
+            this.label9.Text = "Gross Pay";
+            // 
+            // IncometextBox
+            // 
+            this.IncometextBox.Location = new System.Drawing.Point(348, 278);
+            this.IncometextBox.Name = "IncometextBox";
+            this.IncometextBox.ReadOnly = true;
+            this.IncometextBox.Size = new System.Drawing.Size(160, 20);
+            this.IncometextBox.TabIndex = 8;
+            this.IncometextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(239, 278);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(87, 16);
+            this.label10.TabIndex = 3;
+            this.label10.Text = "Income Tax";
+            // 
+            // NettextBox
+            // 
+            this.NettextBox.Location = new System.Drawing.Point(348, 304);
+            this.NettextBox.Name = "NettextBox";
+            this.NettextBox.ReadOnly = true;
+            this.NettextBox.Size = new System.Drawing.Size(160, 20);
+            this.NettextBox.TabIndex = 9;
+            this.NettextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(236, 304);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(80, 16);
+            this.label11.TabIndex = 3;
+            this.label11.Text = "Net Salary";
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // errorProvider2
+            // 
+            this.errorProvider2.ContainerControl = this;
+            // 
+            // errorProvider3
+            // 
+            this.errorProvider3.ContainerControl = this;
+            // 
+            // errorProvider4
+            // 
+            this.errorProvider4.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.NettextBox);
+            this.Controls.Add(this.IncometextBox);
+            this.Controls.Add(this.GrosstextBox);
+            this.Controls.Add(this.HousetextBox);
+            this.Controls.Add(this.MedtextBox);
+            this.Controls.Add(this.BasictextBox);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.DestextBox);
+            this.Controls.Add(this.ContextBox);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.NametextBox);
+            this.Controls.Add(this.IdtextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox IdtextBox;
+        private System.Windows.Forms.TextBox NametextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox DestextBox;
+        private System.Windows.Forms.TextBox BasictextBox;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox ContextBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox MedtextBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox HousetextBox;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox GrosstextBox;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox IncometextBox;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox NettextBox;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.ErrorProvider errorProvider2;
+        private System.Windows.Forms.ErrorProvider errorProvider3;
+        private System.Windows.Forms.ErrorProvider errorProvider4;
+    }
+}
+
---
+++ Employee PayRoll/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Employee_PayRoll
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            int CA, MA, HR,groosPay,IT,netSalary;
+            int basicpay = Convert.ToInt32(BasictextBox.Text);
+            if(basicpay >= 40000)
+            {
+                CA = (int)(basicpay * 0.40);
+                ContextBox.Text = CA.ToString();
+                MA = (int)(basicpay * 0.30);
+                MedtextBox.Text = MA.ToString();
+                HR = (int)(basicpay * 0.20);
+                HousetextBox.Text = HR.ToString();
+                groosPay = basicpay + CA + MA +HR;
+                GrosstextBox.Text = groosPay.ToString();
+                if(groosPay >= 60000)
+                {
+                    IT = (int)(groosPay * 0.03);
+                    IncometextBox.Text = IT.ToString();
+                    netSalary = groosPay -IT;
+                    NettextBox.Text = netSalary.ToString(); 
+                }
+            }
+            else if(basicpay >=
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(IdtextBox.Text))
+            {
+                IdtextBox.Focus();
+                errorProvider1.SetError(this.IdtextBox, " Please Enter the Id");
+            }
+            else
+            {
+                errorProvider1.Clear();
+            }
+        }
+
+        private void NametextBox_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(NametextBox.Text))
+            {
+                NametextBox.Focus();
+                errorProvider2.SetError(this.NametextBox, " Please Enter the Name");
+            }
+            else
+            {
+                errorProvider2.Clear();
+            }
+
+        }
+
+        private void DestextBox_Layout(object sender, LayoutEventArgs e)
+        {
+
+        }
+
+        private void DestextBox_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(DestextBox.Text))
+            {
+                DestextBox.Focus();
+                errorProvider3.SetError(this.DestextBox, " Please Enter the designation");
+            }
+            else
+            {
+                errorProvider3.Clear();
+            }
+        }
+
+        private void BasictextBox_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(BasictextBox.Text))
+            {
+                BasictextBox.Focus();
+                errorProvider4.SetError(this.BasictextBox, " Please Enter the basic Pay");
+            }
+            else
+            {
+                errorProvider4.Clear();
+            }
+        }
+    }
+}
